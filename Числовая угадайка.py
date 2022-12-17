077d6d58dc2d864f9893a10019d9c582fb1836f3
Иоанн <bubvanya@mail.ru>
2022-12-17 23:39:01 +0300

    First small project

---
+++ Числовая угадайка.py
@@ -0,0 +1,46 @@
+# Числовая угадайка
+
+import random
+
+print('Добро пожаловать в числовую угадайку!')
+print('Хотите сыграть? Введите "Да" или "Нет"')
+agree = input()
+if agree == 'Нет':
+    print('Ничего страшного, просто вам это не интересно :)')
+while agree == 'Да':
+    print('До какого числа вы хотите угадывать?')
+    right_value = int(input())
+    n = random.randint(1, right_value)
+    print('Тогда приступим! Введите число:')
+
+
+    def is_valid(answer):
+        if answer.isdigit():
+            if 1 <= int(answer) <= right_value:
+                return True
+        return False
+
+
+    answer = input()
+    counter = 0
+    while is_valid(answer) is not True:
+        print('А может быть все-таки введем целое число от 1 до числа, которое вы сами назвали?')
+        answer = input()
+    while is_valid(answer) is True:
+        if int(answer) > n:
+            print('Ваше число больше загаданного, попробуйте еще разок')
+            counter += 1
+            answer = input()
+        elif int(answer) < n:
+            print('Ваше число меньше загаданного, попробуйте еще разок')
+            counter += 1
+            answer = input()
+        elif int(answer) == n:
+            print('Вы угадали, поздравляем!')
+            print('Попыток:', counter)
+            break
+    print('Хотите сыграть еще разок? Введите "Да" или "Нет":')
+    agree = input()
+    if agree == 'Нет':
+        print('Спасибо, что играли в числовую угадайку!')
+        break
