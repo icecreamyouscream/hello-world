c8290b996ccd32589060955429c46dcf317a43af
icecreamyouscream <bubvanya@mail.ru>
2023-05-05 15:43:09 +0300

    some skillbox task

--- skillbox.py
+++ skillbox.py
@@ -1,15 +1,23 @@
-print("Введите первую точку")
-x1 = float(input('X: '))
-y1 = float(input('Y: '))
+number = int(input('Кол-во человек: '))
+ppl_amt = []
+for ppl in range(number):
+    ppl_amt.append(ppl + 1)
+count_num = int(input('Какое число в считалке? '))
+print('Значит, выбывает каждый ', count_num, '-й человек', sep='')
 
-print("\nВведите вторую точку")
-x2 = float(input('X: '))
-y2 = float(input('Y: '))
-
-x_diff = x1 - x2
-y_diff = y1 - y2
-k = y_diff / x_diff
-b = y2 - k * x2
-
-print("Уравнение прямой, проходящей через эти точки:")
-print("y = ", k, " * x + ", b)
+print('\nТекущий круг людей:', ppl_amt)
+copy_ppl_amt = ppl_amt.copy()
+while True:
+    print('Начало счета с номера', copy_ppl_amt[0])
+    shift = count_num % len(copy_ppl_amt) - 1
+    new_list = copy_ppl_amt[shift:]
+    shift_part = copy_ppl_amt[:shift]
+    new_list.extend(shift_part)
+    print('Выбывает человек под номером', new_list[0])
+    ppl_amt.remove(new_list[0])
+    new_list.remove(new_list[0])
+    copy_ppl_amt = new_list.copy()
+    if len(ppl_amt) == 1:
+        print('\nОстался человек под номером', ppl_amt[0])
+        break
+    print('\nТекущий круг людей:', ppl_amt)
